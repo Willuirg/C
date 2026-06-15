d05478b0b1ca8dcf9197ce9fc9699bbe65d57679
Wil <160080685+Willuirg@users.noreply.github.com>
2026-06-15 19:00:23 +0000

    ajuste fino

--- equecacaode2grau.c
+++ equecacaode2grau.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
 
 int main()
 {
 float a, b, c, delta, x1, x2;
 printf("Digite o coeficiente a: ");
-scanf("%f",&a);
+scanf("%f", &a);
 printf("Digite o coeficiente b: ");
-scanf("%f",&b);
+scanf("%f", &b);
 printf("Digite o coeficiente c: ");
-scanf("%f",&c);
+scanf("%f", &c);
+
+if (a == 0) {
+    printf("Erro: O coeficiente 'a' não pode ser zero!\n");
+    return 1;
+}
 
 delta = b * b - 4 * a * c;
+
+printf("Delta (Δ) = %.2f\n", delta);
 
 if (delta < 0) {
     printf("A equação não tem raízes reais.\n");
